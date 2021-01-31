c41de3c9f2bf289a1a3ea50f6138d3996ae815fd
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-01-31 16:55:57 +0200

    last modifited

--- lab04.js
+++ lab04.js
@@ -1,25 +1,35 @@
+function welcome (userName){
 alert('Welcome to you at my page');
 var userName = prompt('What is your name?');
 while (userName=="")
 {userName = prompt('Please enter your name');
 }
-
 alert('Welcome '+ userName );
+}
+welcome ();
 
-var color = confirm('Are you prefer to change page color ?');
-if(color == true){
-    var colorchange =prompt('What color do you prefer to be the page color?','input color name');
-    document.body.style.backgroundColor = colorchange;
-    alert (' The color will be '+colorchange +'\n Enjoy reading');
-}
-else{
-    alert('As you want \n Welcome to you again');
-}
-alert ('I want to show you a flower image')
+//function backgroundcolor (color , colorchange){
+
+//var color = confirm('Are you prefer to change page color ?');
+//if(color == true){
+ //   var colorchange =prompt('What color do you prefer to be the page color?','input color name');
+  //  document.body.style.backgroundColor = colorchange;
+  //  alert (' The color will be '+colorchange +'\n Enjoy reading');
+//}
+//else{
+  //  alert('As you want \n Welcome to you again');
+//}
+//}
+//backgroundcolor();
+
+function flowershow (userOrder , userName)
+{
+alert ('I want to show you a flower image');
 var userOrder =prompt ('Which kind of flower do you prefer to see tulipes or sunflower?');
+
 while (userOrder !== 'tulipes' && userOrder !=='sunflower')
 {
-    userOrder = prompt ('please enter the correct kind')
+    userOrder = prompt ('please enter the correct kind');
 }
  var userInput = prompt (' How many images do you want ?');
  var image= '';
@@ -34,6 +44,9 @@ while (userOrder !== 'tulipes' && userOrder !=='sunflower')
  
     }
     document.write(image);
+}
+flowershow ();
+
 
 
 
